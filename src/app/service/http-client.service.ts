53f786e2e75d2ce89f97b20041129d3479e0e0fe
Dang Tam <delgemoon@Dangs-MacBook-Pro.local>
2019-05-18 11:22:40 +0700

    adding src

---
+++ src/app/service/http-client.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Employee {
+  constructor(
+    public empId:string,
+    public name:string,
+    public designation:string,
+    public salary:string,
+  ) {}
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(
+    private httpClient : HttpClient
+  ) { }
+  getEmployees() {
+    console.log("Test call");
+    return this.httpClient.get<Employee[]>("http://localhost:8080/employees");
+  }
+}
