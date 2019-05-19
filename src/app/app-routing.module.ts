c8b2d42298355de0490025079ced40835dd28dfd
Dang Tam <delgemoon@Dangs-MacBook-Pro.local>
2019-05-19 13:28:47 +0700

    adding login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import {AddEmployeeComponent } from './add-employee/add-employee.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AuthGaurdService } from './service/auth-gaurd.service';
 
 const routes: Routes = [
-  { path: 'employees', component : EmployeeComponent },
-  { path:'addemployee', component: AddEmployeeComponent},
+  { path: 'employees', component : EmployeeComponent, canActivate: [AuthGaurdService] },
+  { path:'addemployee', component: AddEmployeeComponent, canActivate: [AuthGaurdService]},
+  { path: 'login', component : LoginComponent},
+  { path: 'logout', component : LogoutComponent, canActivate: [AuthGaurdService] },
 ];
 
 @NgModule({
